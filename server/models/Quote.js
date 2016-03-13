7cd1225cfef0007c28a5eaec86d50960deeee558
Scott Hulme <scott.r.hulme@live.co.uk>
2016-03-13 01:25:14 +0000

    fixing issue with database initialisation data

--- server/models/Quote.js
+++ server/models/Quote.js
@@ -12,13 +12,13 @@ var Quote = mongoose.model('Quote', quoteSchema);
 function createInitialQuotes() {
     Quote.find({}).exec(function(err, collection) {
         if(collection.length === 0) {
-            Quote.Create({quote: 'Me fail english? Thats unpossible', person: 'Ralph Wiggum'});
-            Quote.Create({quote: 'Robots dont say ye', person: 'Hermes Conrad'});
-            Quote.Create({quote: 'Two oil changes for the price of one! Now if I could afford the one, and the car.', person: 'Dr John Zoidberg'});
-            Quote.Create({quote: 'You there! Cut my milk.', person: 'Stewie Griffin'});
-            Quote.Create({quote: 'You tried your best and you failed miserably. The lesson is: Never try', person: 'Homer Simpson'});
-            Quote.Create({quote: 'If we can hit that bullseye the rest of the dominoes will fall like a house of cards. checkmate', person: 'Zapp Brannigan'});
-            Quote.Create({quote: 'I hate Mondays', person: 'Garfield'});
+            Quote.create({quote: 'Me fail english? Thats unpossible', person: 'Ralph Wiggum'});
+            Quote.create({quote: 'Robots dont say ye', person: 'Hermes Conrad'});
+            Quote.create({quote: 'Two oil changes for the price of one! Now if I could afford the one, and the car.', person: 'Dr John Zoidberg'});
+            Quote.create({quote: 'You there! Cut my milk.', person: 'Stewie Griffin'});
+            Quote.create({quote: 'You tried your best and you failed miserably. The lesson is: Never try', person: 'Homer Simpson'});
+            Quote.create({quote: 'If we can hit that bullseye the rest of the dominoes will fall like a house of cards. checkmate', person: 'Zapp Brannigan'});
+            Quote.create({quote: 'I hate Mondays', person: 'Garfield'});
         }
     });
 }
