7f05dd0616e662fb42280255710edffbcd6fc7a7
Simon Wallace <simonwallace@users.noreply.github.com>
2016-03-19 18:00:55 +0000

    Started creating ability to update existing releases
    - Added sections to the View Release page which will be shown / hidden
    depending on scope variables. These will allow you to change the
    description value of the release (you can't yet persist it though).
    - Added new functions to the View Release controller to allow you to
    start, cancel, and submit edits to fields on the page.

--- public/app/release/controllers/ctrlViewRelease.js
+++ public/app/release/controllers/ctrlViewRelease.js
@@ -1,9 +1,44 @@
 angular.module('app').controller('ctrlViewRelease', function($scope, $routeParams, dbProject, dbRelease) {
     
+	//Get the project and release IDs from the route
 	var projectId = $routeParams.projectId;
 	var releaseId = $routeParams.releaseId;
 
+	//Data related to the project that the release is associated with
 	$scope.project = dbProject.getProject(projectId);
 
+	//Data related to the release
 	$scope.release = dbRelease.getRelease(projectId, releaseId);
+
+	//Record whether a field is being edited
+	$scope.edit = {};
+
+	//Store the old value of a field as it is being edited
+	$scope.oldData = {};
+
+	$scope.showEdit = function(field, show) {
+        if (show) {
+	        //Store the old data
+	        $scope.oldData[field] = $scope.release[field];
+    	}
+
+        //Show the edit fields
+        $scope.edit[field] = show;
+    }
+
+    $scope.cancelEdit = function(field) {
+    	//Reset the description
+    	$scope.release[field] = $scope.oldData[field];
+
+    	//Stop editing
+    	$scope.showEdit(field, false);
+    }
+
+    $scope.submitEdit = function(field) {
+    	//Save the description
+    	//...
+
+    	//Stop editing
+    	$scope.showEdit(field, false);
+    }
 });
