85f767cfb1931ccc2e45528e9ac987c4d60bb90b
Scott Hulme <scott.r.hulme@live.co.uk>
2016-03-21 22:40:12 +0000

    Roles
    Adding admin and member roles to projects
    Updating sample data to include new fields

--- server/models/Project.js
+++ server/models/Project.js
@@ -2,7 +2,9 @@ var mongoose = require('mongoose');
 
 var projectSchema = mongoose.Schema({
     name: {type:String, required:'{PATH} is required'},
-    description: {type:String, required:'{PATH} is required'}
+    description: {type:String, required:'{PATH} is required'},
+    admins: {type: [String]},
+    members: {type: [String]}
 });
 
 projectSchema.methods = { };
@@ -12,9 +14,9 @@ var Project = mongoose.model('Project', projectSchema);
 function createInitialProject() {
     Project.find({}).exec(function(err, collection) {
         if(collection.length === 0) {
-            Project.create({name: "SpecSaver", description: "Testing tool."});
-            Project.create({name: "Contact360", description: "Customer contact platform."});
-            Project.create({name: "NIROS", description: "Backend processing for registering all life events."});
+            Project.create({name: "SpecSaver", description: "Testing tool.", members: ["Zeroish", "Batman"]});
+            Project.create({name: "Contact360", description: "Customer contact platform.", members: ["Superman", "Batman"]});
+            Project.create({name: "NIROS", description: "Backend processing for registering all life events.", members: ["Zeroish", "IronMan", "TheCaptain"]});
         }
     });
 }
