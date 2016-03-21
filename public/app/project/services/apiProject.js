a3454c52b131af46324c0a83979f4c3da3180e4c
Scott Hulme <scott.r.hulme@live.co.uk>
2016-03-21 23:18:16 +0000

    Project Authorisation
    Users are now only able to see projects of which they are a member or an
    admin.

--- public/app/project/services/apiProject.js
+++ public/app/project/services/apiProject.js
@@ -1,9 +1,25 @@
-angular.module('app').factory('apiProject', function($resource) {
+angular.module('app').factory('apiProject', function($resource, mvIdentity) {
     var projectResource = $resource('/api/projects/:projectId', {projectId: "@projectId"}, {
         query: {method: 'GET', isArray: true, cancellable: true},
         queryOne: {method: 'GET', isArray: false, cancellable: true},
         update: {method: 'PUT', isArray: false}
     });
+    
+    projectResource.prototype.isAdmin = function() {
+        if (mvIdentity.currentUser !== undefined) {
+            return this.admins && this.admins.indexOf(mvIdentity.currentUser.username) > -1;
+        } else {
+            return false;
+        }
+    }
+    
+    projectResource.prototype.isMember = function() {
+        if (mvIdentity.currentUser !== undefined) {
+            return this.members && this.members.indexOf(mvIdentity.currentUser.username) > -1;
+        } else {
+            return false;
+        }
+    }
     
     return projectResource;
 });
