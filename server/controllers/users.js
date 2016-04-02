9e9af9cc959066a0dfe2ee8282c0d595dbee62e1
Scott Hulme <scott.r.hulme@live.co.uk>
2016-04-02 14:58:17 +0100

    View Updates
    Removing quotes section from menu
    Adding selection of first name and last name
    Fixing logout link in nav bar user options

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,7 +1,7 @@
 var User = require('mongoose').model('User');
 
 exports.getUsers = function(req, res) {
-    User.find({}).select('username').exec(function(err, users) {
+    User.find({}, 'username firstname lastname').exec(function(err, users) {
         res.send(users);
     })
 };
