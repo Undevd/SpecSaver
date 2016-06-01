c731d9d6e62bfd20d3621b4c0f88b218f07761f3
Simon Wallace <simonwallace@users.noreply.github.com>
2016-06-01 19:00:01 +0100

    Started implementing 'reorder test steps'
    - bower.json: Added the 'ng-sortable' package to allow test steps to be
    reordered by dragging and dropping.
    - app.js, layout.jade, scripts.jade: Added 'ng-sortable' dependencies.
    - view-system-test: Slightly restructured the test step section to allow
    steps to be reordered and added some of the 'ng-sortable' attributes.
    - site.styl: Added some new rules to style draggable rows and
    parameterised 'spacing' to reduce magic numbers.

--- public/app/app.js
+++ public/app/app.js
@@ -1,4 +1,4 @@
-angular.module('app', ['ngResource', 'ngRoute']);
+angular.module('app', ['ngResource', 'ngRoute', 'as.sortable']);
 
 angular.module('app').config(function($routeProvider, $locationProvider) {
     $locationProvider.html5Mode(true);
