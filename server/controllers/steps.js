5801c2ed332027bdcfdadaae1197e8af91684915
Simon Wallace <simonwallace@users.noreply.github.com>
2016-08-10 19:23:01 +0100

    Import SpecFlow steps
    steps.js: importSpecFlowSteps(): Started creating a method to import
    SpecFlow steps. The JSON which you supply comes from the
    specflow-stepmap cache file which is generated in the %TEMP% directory
    after opening a SpecFlow project in Visual Studio.

--- server/controllers/steps.js
+++ server/controllers/steps.js
@@ -52,6 +52,127 @@ exports.getSteps = function(req, res) {
     })
 };
 
+//Imports the supplied SpecFlow steps
+exports.importSpecFlowSteps = function(request, response) {
+
+    //The start and end placeholder regex characters
+    var START_REGEX = '(';
+    var END_REGEX = ')';
+
+    //The start and end placeholder parameter characters
+    var START_PARAMETER = '{';
+    var END_PARAMETER = '}';
+    
+    //Get the data from the request
+    var data = request.body;
+
+    //If data was supplied
+    if (data) {
+
+        //Store an array of the populated promises
+        var promises = [];
+
+        //Get the file step definitions
+        var files = data.ProjectStepDefinitions.FileStepDefinitions;
+
+        //If any files exist
+        if (files) {
+
+            //For each file
+            for (var file of files) {
+                
+                //Get the step definitions
+                var steps = file.StepDefinitions;
+
+                //If any steps exist
+                if (steps) {
+
+                    //For each step
+                    for (var step of steps) {
+
+                        //Get the step type
+                        var type = step.StepDefinitionType;
+
+                        //Convert the type from an integer into a string
+                        switch (type) {
+
+                            case 1: type = 'Given'; break;
+                            case 2: type = 'When'; break;
+                            case 3: type = 'Then'; break;
+                        }
+
+                        //Get the step pattern
+                        var pattern = step.Regex.pattern;
+
+                        //Get the parameters
+                        var parameters = step.Parameters;
+
+                        //If any parameters exist
+                        if (parameters) {
+                            
+                            //For each parameter
+                            for (var parameter of parameters) {
+
+                                //Get the parameter name
+                                var name = parameter.ParameterName;
+
+                                //Get the parameter type
+                                var parameterType = parameter.Type.FullName;
+
+                                //If the parameter type is for a SpecFlow table
+                                if (parameterType == 'TechTalk.SpecFlow.Table') {
+
+                                    //TBC
+                                    //...
+                                }
+                                else {
+                                    
+                                    //Get the start and end positions of the first regex placeholder
+                                    //These are enclosed in brackets
+                                    var startPos = pattern.indexOf(START_REGEX);
+                                    var endPos = pattern.indexOf(END_REGEX);
+
+                                    //If a placeholder exists
+                                    if (startPos > -1 && endPos > startPos) {
+
+                                        //Replace the placeholder with the parameter name
+                                        //e.g. When I click '(.*)' -> When I click '{button}'
+                                        pattern = pattern.substring(0, startPos)
+                                            + START_PARAMETER
+                                            + name
+                                            + END_PARAMETER
+                                            + pattern.substring(endPos + 1, pattern.length);
+                                    }
+                                }
+                            }
+                        }
+                        
+                    }
+                }
+            }
+        }
+
+        //If no valid data has been supplied
+        if (!promises.length) {
+
+            //Return an error
+            response.status(400).send({code: 400, message: 'No valid data supplied'});
+        }
+
+        //If all the promises are successful
+        Promise.all(promises).then(function(responses) {
+            
+            //Set and send the success status
+            response.sendStatus(200);
+
+        }, function(error) {
+            
+            //Otherwise, set the error status and send the error message
+            response.status(error.code == 404 ? 404 : 400).send({code: error.code, message: error.errmsg});
+        });
+    }
+};
+
 //Searches for a test step using the supplied Project Code, Type, and Step criteria
 exports.searchForStep = function(request, response) {
 
